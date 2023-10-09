8ef3e6aac05eb1b78d3d7903f7db486737644cd4
Ryan Gibb <ryan@freumh.org>
2023-10-09 18:40:27 +0100

    fix template IPv6 addr and enable dns

--- template/configuration.nix
+++ template/configuration.nix
@@ -57,10 +57,11 @@
     username = "user";
     secretsDir = "/secrets";
     serverIpv4 = "203.0.113.0";
-    serverIpv6 = "2001:DB8::/64";
-    publicInterface = "eth0";
+    serverIpv6 = "2001:DB8:0:0:0:0:0:0";
+    publicInterface = "enp1s0";
 
     # TODO enable desired services
+    dns.enable = true;
     # mailserver.enable = true;
     # matrix.enable = true;
     # turn.enable = true;
