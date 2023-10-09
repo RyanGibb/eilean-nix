48013400d6b540afdea7e554c2c98433be688b6a
Ryan Gibb <ryan@freumh.org>
2023-10-09 18:40:49 +0100

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosModules.default = {
