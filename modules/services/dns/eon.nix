bba3719f32b57b853247e299ea44500938d69166
Ryan Gibb <ryan@freumh.org>
2024-11-26 13:33:05 +0000

    eon set DKIM record

--- modules/services/dns/eon.nix
+++ modules/services/dns/eon.nix
@@ -13,4 +13,30 @@ in lib.mkIf (cfg.enable && cfg.server == "eon") {
         }/${zonename}";
     in lib.attrsets.mapAttrsToList mapZonefile cfg.zones;
   };
+
+  users.users = { eon.extraGroups = [ config.services.opendkim.group ]; };
+
+  ### bind prestart copy zonefiles
+  systemd.services.eon.postStart = let
+    update = ''
+      update() {
+        local file="$1"
+        local domain="$2"
+        local input=$(tr -d '\n' < "$file")
+        local record_name=$(echo "$input" | ${pkgs.gawk}/bin/awk '{print $1}')
+        local record_type=$(echo "$input" | ${pkgs.gawk}/bin/awk '{print $3}')
+        local ttl=3600
+        local record_value=$(echo "$input" | ${pkgs.gnused}/bin/sed -E 's/[^"]*"([^"]*)"[^"]*/\1/g')
+        ${config.services.eon.package}/bin/capc update /var/lib/eon/caps/domain/''${domain}.cap -u add:''${record_name}.''${domain}:''${record_type}:"''${record_value}":''${ttl} || exit 0
+      }
+      shopt -s nullglob
+    '';
+    ops = let
+      mapZones = zonename: zone: ''
+        for f in ${config.mailserver.dkimKeyDirectory}/${zonename}.*.txt; do
+          update $f ${zonename}
+        done
+      '';
+    in lib.attrsets.mapAttrsToList mapZones cfg.zones;
+  in update + builtins.concatStringsSep "\n" ops;
 }
